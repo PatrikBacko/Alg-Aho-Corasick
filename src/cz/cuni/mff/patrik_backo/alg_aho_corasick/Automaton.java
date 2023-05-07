20499296e6c5ede1c01f144308d6d91cd6d5d401
Patrik Backo <llama8989@gmail.com>
2023-05-07 13:23:49 +0200

    Automaton probably done, needs some testing

--- src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Automaton.java
+++ src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Automaton.java
@@ -1,20 +1,56 @@
 package cz.cuni.mff.patrik_backo.alg_aho_corasick;
 
 import java.util.HashMap;
-
-// import cz.cuni.mff.patrik_backo.alg_aho_corasick.Node;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Collections;
 
 public class Automaton{
     Node root;
     Node currState;
+    final List<String> emptyList;
+
+    public Automaton(){
+        root = new Node();
+        currState = root;
+        emptyList = new ArrayList<>();
+    }
+
+    static public Automaton buildAutomaton(String[] words){
+        Automaton automaton = new Automaton();
+        automaton.buildTrie(words);
+        automaton.buildShortcuts();
+        return automaton;
+    }
+
+    public void stepForward(char c){
+        currState = step(currState, c);
+    }
+ 
+    public List<String> getWords(){
+        Node currNode = currState;
+        if ((!currNode.isWord) && (currNode.shortcut == null)) 
+            return Collections.unmodifiableList(emptyList);
+
+        List<String> words = new ArrayList<>();
+        
+        if(currNode.isWord){
+            words.add(currNode.prefix);
+        }
+
+        while(currNode.shortcut != null){
+            currNode = currNode.shortcut;
+        }
+        return Collections.unmodifiableList(words);
+    }
 
     private class Node{
         HashMap<Character, Node> children;
         String prefix;
         Node shortcut;
         Node trailingEdge;
-        Node parent;
         boolean isWord;
     
         Node(){
@@ -22,65 +58,59 @@ public class Automaton{
             prefix = "";
             shortcut = null;
             trailingEdge = null;
-            parent = null;
             isWord = false;
         }
     }
 
-    public Automaton(){
-        root = new Node();
-        currState = root;
-    }
-
-    static public Automaton buildAutomaton(String[] words){
-        Automaton automaton = new Automaton();
-        automaton.buildTrie(words);
-        automaton.buildShortcuts(words);
-        return automaton;
-    }
-
-    private void buildShortcuts(String[] words){
-        Node currNode = root;
-        int maxWordsLenght = 0;
-        for(String word : words){
-            maxWordsLenght = Math.max(maxWordsLenght, word.length());
-        }
-        
-        for (int i = 0; i < maxWordsLenght; i++){
-            for(String word : words){
-                if(i >= word.length()) continue;
-                char c = word.charAt(i);
-                stepForward(c);
+    private void buildShortcuts(){
+        Deque<Node> queue = new LinkedList<>();
+        root.trailingEdge = root;
+        root.shortcut = null;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node currNode = queue.remove();
+            for(Node child : currNode.children.values()){
+                Node node = step(currNode.trailingEdge, child.prefix.charAt(child.prefix.length() - 1));
+                child.trailingEdge = node;
+                if(node.isWord){
+                    child.shortcut = node;
+                }
+                else{
+                    child.shortcut = node.shortcut;
+                }
+                queue.add(child);
             }
         }
     }
 
-    private void stepForward(char c){
-        while (currState != root){
-            if(currState.children.containsKey(c)){
-                currState = currState.children.get(c);
-                return;
+    private Node step(Node node, char c){
+        while (node != root){
+            if(node.children.containsKey(c)){
+                return node.children.get(c);
             }
             else{
-                currState = currState.trailingEdge;
+                node = node.trailingEdge;
             }
         }
+        if(node.children.containsKey(c)){
+            return node.children.get(c);
+        }
+        return root;
     }
 
     private void buildTrie(String[] words){
         for(String word : words){
-            insert(word);
+            insertWord(word);
         }
     }
 
-    private Node insert(String word){
+    private Node insertWord(String word){
         Node current_node = root;
         for(int i = 0; i < word.length(); i++){
             char c = word.charAt(i);
             if(!current_node.children.containsKey(c)){
                 Node new_node = new Node();
                 new_node.prefix = current_node.prefix + c;
-                new_node.parent = current_node;
                 current_node.children.put(c, new_node);
             }
             current_node = current_node.children.get(c);
