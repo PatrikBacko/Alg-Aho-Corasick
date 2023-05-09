85a80f1b2aa32e0737688194707ce4ad6ed116df
Buciak <patrik.backo1@gmail.com>
2023-05-09 14:30:15 +0200

    .

--- src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Main.java
+++ src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Main.java
@@ -5,17 +5,19 @@ import cz.cuni.mff.patrik_backo.alg_aho_corasick.algorithms.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args){
         args = new String[]{"abc"};
         HashMap<String, String> wordMap = new HashMap<String, String>();
+        Reader reader= new InputStreamReader(System.in);
         wordMap.put("abc", "***");
         // Algorithm algorithm = new Counter(new InputStreamReader(System.in), new PrintWriter(System.out), args);
         // Algorithm algorithm2 = new SignalPossition(new InputStreamReader(System.in), new PrintWriter(System.out), args);
         // Algorithm algorithm3 = new Censor(new InputStreamReader(System.in), new PrintWriter(System.out), args);
-        Algorithm algorithm4 = new Replacer(new InputStreamReader(System.in), new PrintWriter(System.out), args, wordMap);
+        Algorithm algorithm4 = new Replacer(reader, new PrintWriter(System.out), args, wordMap);
 
         try {
             // algorithm.run();
