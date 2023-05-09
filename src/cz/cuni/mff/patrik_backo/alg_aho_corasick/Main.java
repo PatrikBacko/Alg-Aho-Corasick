510e5eced4c3e58fe05f2c8092e352fd19f7a08e
Buciak <patrik.backo1@gmail.com>
2023-05-09 10:10:45 +0200

    Replacer Done
    TODO:
    - parser
    - documentation
    - final testing

--- src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Main.java
+++ src/cz/cuni/mff/patrik_backo/alg_aho_corasick/Main.java
@@ -5,17 +5,23 @@ import cz.cuni.mff.patrik_backo.alg_aho_corasick.algorithms.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args){
-        args = new String[]{"abc","cde","fgh"};
+        args = new String[]{"abc"};
+        HashMap<String, String> wordMap = new HashMap<String, String>();
+        wordMap.put("abc", "***");
         // Algorithm algorithm = new Counter(new InputStreamReader(System.in), new PrintWriter(System.out), args);
         // Algorithm algorithm2 = new SignalPossition(new InputStreamReader(System.in), new PrintWriter(System.out), args);
-        Algorithm algorithm3 = new Censor(new InputStreamReader(System.in), new PrintWriter(System.out), args);
+        // Algorithm algorithm3 = new Censor(new InputStreamReader(System.in), new PrintWriter(System.out), args);
+        Algorithm algorithm4 = new Replacer(new InputStreamReader(System.in), new PrintWriter(System.out), args, wordMap);
+
         try {
             // algorithm.run();
             // algorithm2.run();
-            algorithm3.run();
+            // algorithm3.run();
+            algorithm4.run();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/cz/cuni/mff/patrik_backo/alg_aho_corasick/algorithms/Replacer.java
+++ src/cz/cuni/mff/patrik_backo/alg_aho_corasick/algorithms/Replacer.java
@@ -1,15 +1,51 @@
 package cz.cuni.mff.patrik_backo.alg_aho_corasick.algorithms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.Reader;
+
 
 public class Replacer extends Algorithm{
-    private HashMap<String, String> map;
+    private HashMap<String, String> wordMap;
 
-    public Replacer(Reader text, PrintWriter output, String[] words, HashMap<String, String> map){
+    public Replacer(Reader text, PrintWriter output, String[] words, HashMap<String, String> wordMap){
         super(text, output, words);
-        this.map = map;
+        this.wordMap = wordMap;
     }
 
     public PrintWriter run() throws IOException{
+        int c;
+        List<String> buffer = new ArrayList<String>();
+        while((c = text.read()) != -1){
+            buffer.add(Character.toString((char) c));
+            automaton.stepForward((char) c);
+            
+            List<String> list = automaton.getWords();
+
+            if(list.size() > 0){
+                String word = list.get(0);
+                for(int i = 0; i < word.length(); i++){
+                    buffer.remove(buffer.size() - 1);
+                }
+                buffer.add(wordMap.get(word));
+                automaton.resetState();
+            }
+
+            if (automaton.isRoot()){
+                for (String word : buffer) {
+                    output.print(word);
+                }
+                buffer.clear();
+            }
+        }
+            for (String word : buffer) {
+                output.print(word);
+            }
+        output.flush();
+        return output;
+        
     }
 }
